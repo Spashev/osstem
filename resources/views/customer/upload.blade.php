eecb4576a7986f890db936c22fbd3d1dae70d83d
Spashev_Nurken <s.nurken92@gmail.com>
2020-10-22 23:49:18 +0600

    fix

--- resources/views/customer/upload.blade.php
+++ resources/views/customer/upload.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="col-lg-8 col-xl-5">
                     <!-- DropzoneJS Container -->
-                <form class="dropzone dz-clickable" action="{{route('admin.import')}}" method="POST">
+                <form class="dropzone dz-clickable" action="{{route('admin.import')}}" method="POST" enctype="multipart/form-data">
                     @csrf
                         <div class="dz-default dz-message"><span>Drop files here to upload</span></div>
                     </form>
